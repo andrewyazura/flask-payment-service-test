a7ac0bab9b00d0aeb3147c0867ec0e0a8d7a0daf
Andrew Yatsura <andrewyazura203@gmail.com>
2021-07-16 22:14:01 +0300

    Add logging

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests
 from flask import Blueprint, current_app, redirect, render_template
 
@@ -13,6 +15,8 @@ def homepage():
     form = PaymentForm()
 
     if form.validate_on_submit():
+        logging.info('valid payment form received')
+
         data = form.data
         payment = PaymentModel(
             amount=str(data['amount']),
@@ -21,8 +25,10 @@ def homepage():
         )
         db.session.add(payment)
         db.session.commit()
+        logging.debug('payment created')
 
         if data['currency'] == '840':
+            logging.info('form currency is USD')
             required_data = {
                 'shop_id': current_app.config['SHOP_ID'],
                 'shop_amount': str(data['amount']),
@@ -36,14 +42,22 @@ def homepage():
             response = requests.post(
                 'https://core.piastrix.com/bill/create', json=data
             ).json()
+            logging.info('request to bill api sent')
 
             if response['result']:
-
+                logging.info(
+                    'request to bill api successful, redirecting user to received url'
+                )
                 return redirect(response['data']['url'])
             else:
-                print(response['message'])
+                logging.error(
+                    'request to api failed, error message: {}'.format(
+                        response['message']
+                    )
+                )
 
         elif data['currency'] == '978':
+            logging.info('form currency is EUR')
             required_data = {
                 'amount': str(data['amount']),
                 'currency': int(data['currency']),
@@ -57,14 +71,22 @@ def homepage():
             response = requests.post(
                 'https://core.piastrix.com/invoice/create', json=data
             ).json()
+            logging.info('request to invoice api sent')
 
             if response['result']:
-
+                logging.info(
+                    'request to invoice api successful, redirecting user to invoice form'
+                )
                 return render_template('invoice.html', form_data=response['data'])
             else:
-                print(response['message'])
+                logging.error(
+                    'request to api failed, error message: {}'.format(
+                        response['message']
+                    )
+                )
 
         elif data['currency'] == '643':
+            logging.info('form currency is RUB')
             required_data = {
                 'amount': str(data['amount']),
                 'currency': int(data['currency']),
@@ -75,6 +97,8 @@ def homepage():
             hash = generate_signature(required_data, current_app.secret_key)
             data = {**required_data, 'sign': hash, 'description': data['description']}
 
+            logging.info('redirecting user to payment form')
             return render_template('pay.html', form_data=data)
 
+    logging.info('rendering homepage')
     return render_template('homepage.html', form=form)
