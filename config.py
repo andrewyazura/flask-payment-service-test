045f493094dbeb60f8b1e5cf8d31c5a9e1166f8e
Andrew Yatsura <andrewyazura203@gmail.com>
2021-07-16 22:06:41 +0300

    Create db files in root folder and ignore them

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config(object):
     SECRET_KEY = 'SecretKey01'
     SHOP_ID = 5
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///payments.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../payments.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
